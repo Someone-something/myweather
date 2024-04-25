f61ac8122d7ee1659836a3efe8f749b7d373935a
gaofh <1536287033@qq.com>
2024-04-25 22:52:39 +0800

    提交网络请求类相关信息

--- app/src/main/java/com/gaofh/myweather/util/HttpUtil.java
+++ app/src/main/java/com/gaofh/myweather/util/HttpUtil.java
@@ -1,2 +1,35 @@
-package com.gaofh.myweather.util;public class HttpUtil {
+package com.gaofh.myweather.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtil {
+
+    public static String getHttpResponse(String address){
+        HttpURLConnection urlConnection=null;
+        StringBuilder builder=new StringBuilder();
+        try {
+            URL url=new URL(address);
+            urlConnection=(HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setConnectTimeout(8000);
+            urlConnection.setReadTimeout(8000);
+            InputStream inputStream=urlConnection.getInputStream();
+            BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
+            String line="";
+            while ((line=reader.readLine())!=null){
+                builder.append(line);
+            }
+        }catch (Exception e){
+          e.printStackTrace();
+        }finally {
+            if(urlConnection!=null){
+                urlConnection.disconnect();
+            }
+        }
+       return builder.toString();
+    }
 }
