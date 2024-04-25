7ed66f221a47663b8657f67e195c96cdb86caec3
gaofh <1536287033@qq.com>
2024-04-26 00:04:16 +0800

    提交URL类相关信息

---
+++ app/src/main/java/com/gaofh/myweather/util/UrlUtil.java
@@ -0,0 +1,6 @@
+package com.gaofh.myweather.util;
+
+public class UrlUtil {
+    public static String provinceUrl="http://guolin.tech/api/china";
+    public static String weatherUrl="http://guolin.tech/api/weather";
+}
